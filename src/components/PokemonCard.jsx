b53d8c7feb82f6ca336b0d8fb529319216d75421
Mehdi Ouhab <ouhab.me@gmail.com>
2024-03-28 15:52:32 +0100

    Pokemon added

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,20 @@
 function PokemonCard(){
+    const pokemonList = [
+        {
+            name: "bulbasaur",
+            imgSrc:
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+          },
+          {
+            name: "mew",
+          }
+    ];
+    const pokemon = pokemonList[1];
+
     return(
         <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>Bulbasaur</figcaption>
+            {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>?????</p>}
+            <figcaption>{pokemon.name}</figcaption>
         </figure>
     )
 }

3e6e37cbfed2b1a1a622120ce4e45fec5120f5d1
Mehdi Ouhab <ouhab.me@gmail.com>
2024-04-03 09:48:03 +0200

    React 04

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
 function App() {
+  const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+      },
+      {
+        name: "mew",
+      }
+];
 
   return (
     <>
       <div>
-        <PokemonCard/>
+        <PokemonCard pokemon={pokemonList[1]}/>
       </div>
     </>
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,4 @@
-function PokemonCard(){
-    const pokemonList = [
-        {
-            name: "bulbasaur",
-            imgSrc:
-              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-          },
-          {
-            name: "mew",
-          }
-    ];
-    const pokemon = pokemonList[1];
-
+function PokemonCard({pokemon}){
     return(
         <figure>
             {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} /> : <p>?????</p>}
